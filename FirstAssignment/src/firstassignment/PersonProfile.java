9458ae965e48614e265038154f63c5dd63c20859
unknown <talreja9191@gmail.com>
2021-09-26 15:41:01 -0400

    FirstAssignment

---
+++ FirstAssignment/src/firstassignment/PersonProfile.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstassignment;
+
+/**
+ *
+ * @author talre
+ */
+public class PersonProfile {
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public long getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(long PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public long getFaxNumber() {
+        return FaxNumber;
+    }
+
+    public void setFaxNumber(long FaxNumber) {
+        this.FaxNumber = FaxNumber;
+    }
+
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+
+    public void setEmailAddress(String EmailAddress) {
+        this.EmailAddress = EmailAddress;
+    }
+
+
+    public int getMedicalRecordNumber() {
+        return MedicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(int MedicalRecordNumber) {
+        this.MedicalRecordNumber = MedicalRecordNumber;
+    }
+
+    public int getHealthPlanNumber() {
+        return HealthPlanNumber;
+    }
+
+    public void setHealthPlanNumber(int HealthPlanNumber) {
+        this.HealthPlanNumber = HealthPlanNumber;
+    }
+
+    public long getBankAccountNumber() {
+        return BankAccountNumber;
+    }
+
+    public void setBankAccountNumber(long BankAccountNumber) {
+        this.BankAccountNumber = BankAccountNumber;
+    }
+
+    public int getCertificateNumber() {
+        return CertificateNumber;
+    }
+
+    public void setCertificateNumber(int CertificateNumber) {
+        this.CertificateNumber = CertificateNumber;
+    }
+
+    public String getVehicleIdentifier() {
+        return VehicleIdentifier;
+    }
+
+    public void setVehicleIdentifier(String VehicleIdentifier) {
+        this.VehicleIdentifier = VehicleIdentifier;
+    }
+
+    public String getDeviceIdentifier() {
+        return DeviceIdentifier;
+    }
+
+    public void setDeviceIdentifier(String DeviceIdentifier) {
+        this.DeviceIdentifier = DeviceIdentifier;
+    }
+
+    public String getLinkedinLink() {
+        return LinkedinLink;
+    }
+
+    public void setLinkedinLink(String LinkedinLink) {
+        this.LinkedinLink = LinkedinLink;
+    }
+
+    public String getIpAddress() {
+        return IpAddress;
+    }
+
+    public void setIpAddress(String IpAddress) {
+        this.IpAddress = IpAddress;
+    }
+
+    public String getUniqueIdentification() {
+        return UniqueIdentification;
+    }
+
+    public void setUniqueIdentification(String UniqueIdentification) {
+        this.UniqueIdentification = UniqueIdentification;
+    }
+    private String fullName;
+    private String Address;
+
+    public String getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(String DoB) {
+        this.DoB = DoB;
+    }
+    
+    private String DoB;
+    private long PhoneNumber;
+    private long FaxNumber;
+    private String EmailAddress;
+
+    public String getSocialSecurityNumber() {
+        return SocialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String SocialSecurityNumber) {
+        this.SocialSecurityNumber = SocialSecurityNumber;
+    }
+    private String SocialSecurityNumber;
+    private int MedicalRecordNumber;
+    private int HealthPlanNumber;
+    private long BankAccountNumber;
+    private int CertificateNumber;
+    private String VehicleIdentifier;
+    private String DeviceIdentifier;
+    private String LinkedinLink;
+    private String IpAddress;
+    private String UniqueIdentification;
+
+    public String getPhotoFilePath() {
+        return PhotoFilePath;
+    }
+
+    public void setPhotoFilePath(String PhotoFilePath) {
+        this.PhotoFilePath = PhotoFilePath;
+    }
+     private String PhotoFilePath;
+    
+    
+    
+         
+}
